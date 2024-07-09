45db9c61ba1b5264c424e75e178dd4eb3db716e1
Arouid <scott77598@outlook.com>
2024-07-08 23:17:14 -0500

    add columns array. add nested loop that creates 16x16 flex grid

--- script.js
+++ script.js
@@ -3,15 +3,39 @@ container.id = "flexGrid";
 container.className = "container";
 document.body.append(container);
 
+// row code
+
+const rowarray = []; // holds 16 row divs
 for (i = 0; i < 16; i++) {
-  const rowDiv = document.createElement("div");
-  rowDiv.className = "row";
-  container.append(rowDiv);
+  const row = document.createElement("div");
+  rowarray.push(row);
+  //   rowDiv.className = "row";
+  //   container.append(rowDiv);
 }
 
+// column code
+
+const columnarray = []; // hold the 256 columns
+for (i = 0; i < 16 * 16; i++) {
+  const column = document.createElement("div");
+  columnarray.push(column);
+}
+for (row of rowarray) {
+  rows = rowarray.pop();
+  container.append(rows);
+  rows.className = "row";
+  for (i = 0; i < 16; i++) {
+    column = columnarray.pop();
+    rows.append(column);
+    column.className = "column";
+  }
+}
+
+// console.log(rowarray);
+// console.log(columnarray);
 // const buildrows = () => {
-//   document.createElement("div");
-//   //   document.container.append();
+// document.createElement("div");
+// document.container.append();
 // };
 
 // buildrows();
