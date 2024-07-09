01bf92103383de4b06a007ccbc0ff3730f975a75
Arouid <scott77598@outlook.com>
2024-07-08 23:38:05 -0500

    removed old comments and dead code.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// grid wrapper
+
 const container = document.createElement("div");
 container.id = "flexGrid";
 container.className = "container";
@@ -9,10 +11,7 @@ const rowarray = []; // holds 16 row divs
 for (i = 0; i < 16; i++) {
   const row = document.createElement("div");
   rowarray.push(row);
-  //   rowDiv.className = "row";
-  //   container.append(rowDiv);
 }
-
 // column code
 
 const columnarray = []; // hold the 256 columns
@@ -20,22 +19,17 @@ for (i = 0; i < 16 * 16; i++) {
   const column = document.createElement("div");
   columnarray.push(column);
 }
-for (row of rowarray) {
+
+// Build the 16x16 grid dynamically
+
+for (let k = 0; k < 16; k++) {
   rows = rowarray.pop();
   container.append(rows);
   rows.className = "row";
-  for (i = 0; i < 16; i++) {
+
+  for (let i = 0; i < 16; i++) {
     column = columnarray.pop();
     rows.append(column);
     column.className = "column";
   }
 }
-
-// console.log(rowarray);
-// console.log(columnarray);
-// const buildrows = () => {
-// document.createElement("div");
-// document.container.append();
-// };
-
-// buildrows();
